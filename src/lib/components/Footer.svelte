a588453789d271f4884b37bf451b7afa4f835c9e
ana117 <63540951+ana117@users.noreply.github.com>
2023-07-24 21:29:25 +0700

    Extract navbar and footer to its own component

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,5 @@
+<footer class="py-[2rem] mt-[2rem] md:ml-[8rem] bg-secondary text-center">
+    <p class="text-bg font-semibold text-xl md:text-lg">
+        © 2023 Ahmad Naufan
+    </p>
+</footer>
---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,11 @@
+<script>
+import SidebarIcon from "$lib/components/SidebarIcon.svelte";
+</script>
+
+<nav class="h-screen w-[8rem] top-0 left-0 fixed
+            bg-white shadow-lg px-[1rem]
+            flex flex-col gap-y-[2rem] hidden md:block">
+    <SidebarIcon href="/" icon="logo" alt="logo" hover={false} customClass="w-[7rem] h-[7rem]" />
+    <SidebarIcon href="#projects" icon="projects" alt="projects" tooltip="View Selected Projects" />
+    <SidebarIcon href="#contact" icon="mail" alt="contact" tooltip="Contact Me" />
+</nav>
