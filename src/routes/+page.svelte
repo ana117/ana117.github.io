de8817099cdc23d274c0640f7d553a748959d653
ana117 <63540951+ana117@users.noreply.github.com>
2023-07-24 18:46:07 +0700

    Add title and favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,12 @@
     import Contact from "$lib/components/Contact.svelte";
 </script>
 
+<svelte:head>
+    <title>ana117 | Naufan</title>
+    <link rel="icon" href="/favicon.ico" sizes="any">
+    <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any">
+</svelte:head>
+
 <div class="ml-[8rem] min-h-screen">
     <Hero />
 
