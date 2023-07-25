27c0dd28516b7330e5f226ef382f51e4270ea87e
ana117 <63540951+ana117@users.noreply.github.com>
2023-07-25 15:31:00 +0700

    Make dark mode class-based

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -2,7 +2,8 @@
          class="flex flex-col justify-center items-center py-[1rem] h-screen
                 bg-secondary dark:bg-accent">
     <div class="flex justify-center items-center grow w-fit relative">
-        <img src="/assets/icons/logo_outer.svg" alt="logo" class="h-full">
+        <img src="/assets/icons/logo_outer.svg" alt="logo" class="h-full dark:hidden">
+        <img src="/assets/icons/logo_outer_light.svg" alt="logo" class="h-full hidden dark:block">
         <h1 class="absolute font-bold text-[3rem] md:text-[4rem]
                    text-center text-primary dark:text-secondary whitespace-pre-line">
             <span class="text-[2rem] md:text-[2.5rem] align-top">Greetings Human,</span>
@@ -11,6 +12,8 @@
     </div>
     <a href="#content">
         <img src="/assets/icons/down_arrow.svg" alt="down arrow"
-             class="w-[6rem] md:w-[5rem] grow-0 animate-bounce">
+             class="w-[6rem] md:w-[5rem] grow-0 animate-bounce dark:hidden">
+        <img src="/assets/icons/down_arrow_light.svg" alt="down arrow"
+             class="w-[6rem] md:w-[5rem] grow-0 animate-bounce hidden dark:block">
     </a>
 </section>
--- src/lib/components/NavbarMobile.svelte
+++ src/lib/components/NavbarMobile.svelte
@@ -1,5 +1,6 @@
 <script>
     import SidebarIcon from "$lib/components/SidebarIcon.svelte";
+    import DarkModeSwitch from "$lib/components/DarkModeSwitch.svelte";
 
     let showMenu = false;
 
@@ -12,11 +13,13 @@
     <button on:click={toggle}
             class="flex items-center justify-center bg-primary {showMenu ? 'rounded-b-[3rem]' : 'rounded-[3rem]'}
                    w-[5rem] h-[5rem] fixed bottom-[2rem] right-[1rem] transition-all duration-100 no-highlight">
-        <SidebarIcon href="" icon="logo_white" alt="logo" hover={false}/>
+        <SidebarIcon href="" icon="logo_light" alt="logo" hover={false}/>
     </button>
     <div class="bg-primary rounded-t-3xl {showMenu ? 'scale-100' : 'scale-0'} z-10
-                w-[5rem] fixed bottom-[7.5rem] right-[1rem] px-[1rem] transition-all origin-bottom duration-100">
-        <SidebarIcon href="#projects" icon="projects_white" alt="projects"/>
-        <SidebarIcon href="#contact" icon="mail_white" alt="contact"/>
+                w-[5rem] fixed bottom-[7.5rem] right-[1rem] px-[1rem] pb-[0.5rem]
+                transition-all origin-bottom duration-100">
+        <SidebarIcon href="#projects" icon="projects" alt="projects"/>
+        <SidebarIcon href="#contact" icon="mail" alt="contact"/>
+        <DarkModeSwitch/>
     </div>
 </nav>
