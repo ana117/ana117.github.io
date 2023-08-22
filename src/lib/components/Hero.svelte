3fb0d2d0782e3d85f71166e70a1b5862b01f30bc
ana117 <63540951+ana117@users.noreply.github.com>
2023-08-22 18:01:15 +0700

    Update Hero section

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,19 +1,34 @@
 <section id="hero"
          class="flex flex-col justify-center items-center py-[1rem] h-screen
                 bg-secondary dark:bg-accent">
-    <div class="flex justify-center items-center grow w-fit relative z-0">
-        <img src="/assets/icons/logo_outer.svg" alt="logo" class="h-full dark:hidden">
-        <img src="/assets/icons/logo_outer_light.svg" alt="logo" class="h-full hidden dark:block">
-        <h1 class="absolute font-bold text-[3rem] md:text-[4rem]
-                   text-center text-primary dark:text-secondary whitespace-pre-line">
-            <span class="text-[2rem] md:text-[2.5rem] align-top">Greetings Human,</span>
+    <div class="flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-[4rem]
+                grow relative z-0 px-[4rem] text-primary dark:text-secondary">
+        <img src="/assets/icons/logo.svg" alt="logo" class="w-[16rem] md:w-[32rem] dark:hidden">
+        <img src="/assets/icons/logo_light.svg" alt="logo" class="w-[16rem] md:w-[32rem] hidden dark:block">
+        <div class="lg:hidden text-[2rem] md:text-[3rem] text-center font-bold whitespace-pre-line">
+            Greetings!
             I'm Naufan
-        </h1>
+        </div>
+        <div class="hidden lg:flex flex-col gap-[1rem] ">
+            <h1 class="font-bold text-[3rem] lg:text-[3rem]">
+                Greetings Human, I'm Naufan
+            </h1>
+            <p class="text-[2rem] whitespace-pre-line">
+                A final year Computer Science student
+                with a passion for programming, particularly in <span class="font-semibold">web development</span>.
+            </p>
+            <a href="#contact"
+               class="text-[1.25rem] italic  w-fit
+                      decoration-secondary underline-offset-8 underline
+                      hover:bg-secondary hover:text-primary">
+                Open to new opportunities
+            </a>
+        </div>
     </div>
     <a href="#content">
         <img src="/assets/icons/down_arrow.svg" alt="down arrow"
-             class="w-[6rem] md:w-[5rem] grow-0 animate-bounce dark:hidden">
+             class="w-[6rem] lg:w-[5rem] grow-0 animate-bounce dark:hidden">
         <img src="/assets/icons/down_arrow_light.svg" alt="down arrow"
-             class="w-[6rem] md:w-[5rem] grow-0 animate-bounce hidden dark:block">
+             class="w-[6rem] lg:w-[5rem] grow-0 animate-bounce hidden dark:block">
     </a>
 </section>
