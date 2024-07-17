bf34f3e2fa0cb55f7191b034a089ac550c998d27
ana117 <63540951+ana117@users.noreply.github.com>
2024-07-17 18:42:25 +0700

    Move projects data to JS file

---
+++ src/lib/data/projects.js
@@ -0,0 +1,102 @@
+const projects = [
+    {
+      project: "cloudcipher",
+      title: "CloudCipher",
+      summary: "End-to-end encrypted cloud storage desktop app",
+      description: "An end-to-end encrypted cloud storage. Implementing proxy re-encryption for secure file sharing.\nProject for Undergraduate Thesis.",
+      techStacks: [
+        "java",
+        "javafx",
+        "springboot",
+        "aws",
+      ],
+      github: "https://github.com/cloudcipher-ui/"
+    },
+    {
+      project: "imdifoods",
+      title: "Imdifoods",
+      summary: "E-commerce website for a food business in Indonesia",
+      description: "E-commerce website for a food business in Indonesia.\nGroup project for Software Engineering course.",
+      techStacks: [
+        "java",
+        "springboot",
+        "postgre",
+        "tailwind"
+      ]
+    },
+    {
+      project: "go-chat",
+      title: "GoChat",
+      summary: "A simple real-time chat application using Go and WebSockets.",
+      description: "Real-time chat application using Go and WebSockets. Features include multiple chat rooms. Made using Go.",
+      techStacks: [
+        "go",
+        "tailwind"
+      ],
+      github: "https://github.com/ana117/go-chat"
+    },
+    {
+      project: "kordict",
+      title: "Korean Dictionary",
+      summary: "Korean-English dictionary web app using Korean Dictionary API",
+      description: "Korean-English dictionary web app using Korean Dictionary API. Made using ReactJS and Django for the proxy server.",
+      techStacks: [
+        "react",
+        "python",
+        "tailwind"
+      ],
+      github: "https://github.com/ana117/korean-dictionary",
+      website: "https://ana117.github.io/korean-dictionary/"
+    },
+    {
+      project: "web-games",
+      title: "Web Games",
+      summary: "Collections of Javascript browser-based games",
+      description: "Collections of my Javascript browser-based games. Made using ReactJS.",
+      techStacks: [
+        "react",
+        "tailwind"
+      ],
+      github: "https://github.com/ana117/web-games",
+      website: "https://ana117.github.io/web-games/"
+    },
+    {
+      project: "grooming-gallery",
+      title: "The Grooming Gallery Barbershop",
+      summary: "Barbershop website to showcase their services and products",
+      description: "Barbershop website to showcase their services and products (not a real business, demonstration only). Made using ReactJS.",
+      techStacks: [
+        "react",
+        "tailwind"
+      ],
+      github: "https://github.com/ana117/grooming-gallery",
+      website: "https://ana117.github.io/grooming-gallery/"
+    },
+    {
+      project: "konvasearch",
+      title: "KonvaSearch",
+      summary: "Platform to find and donate convalescent plasma for COVID-19 patients",
+      description: "Web and mobile app to find and donate convalescent plasma for COVID-19 patients.\nGroup project Platform-based Programming course.",
+      techStacks: [
+        "python",
+        "flutter",
+        "postgre"
+      ],
+      github: "https://github.com/orgs/Pejuang-PBP/repositories"
+    },
+    {
+      project: "sifost",
+      title: "SIFOST",
+      summary: "Survey and form maker web app",
+      description: "Survey and form maker web app made in Monolithic and Microservice architecture.\nGroup project for Advanced Programming course.",
+      techStacks: [
+        "python",
+        "flutter",
+        "postgre"
+      ],
+      github: "https://github.com/ana117/sifost"
+    }
+  ];
+  
+  export default projects;
+  
