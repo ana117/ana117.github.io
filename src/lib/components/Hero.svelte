297ad07df7e05606ac4a84bd06363647437fa259
ana117 <63540951+ana117@users.noreply.github.com>
2024-07-24 11:09:31 +0700

    Revamp hero section

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -3,26 +3,38 @@
                 bg-secondary dark:bg-accent">
     <div class="flex flex-col lg:flex-row justify-center lg:justify-between items-center gap-[4rem]
                 grow relative z-0 px-[4rem] text-primary dark:text-secondary">
-        <img src="/assets/icons/logo.svg" alt="logo" class="w-[16rem] md:w-[32rem] dark:hidden">
-        <img src="/assets/icons/logo_light.svg" alt="logo" class="w-[16rem] md:w-[32rem] hidden dark:block">
-        <div class="lg:hidden text-[2rem] md:text-[3rem] text-center font-bold whitespace-pre-line">
-            Greetings!
-            I'm Naufan
-        </div>
-        <div class="hidden lg:flex flex-col gap-[1rem] ">
-            <h1 class="font-bold lg:text-4xl">
-                Greetings Human, I'm Naufan
+                
+        <img src="/assets/icons/logo.svg" alt="logo" class="w-[16rem] xl:w-[32rem] dark:hidden">
+        <img src="/assets/icons/logo_light.svg" alt="logo" class="w-[16rem] xl:w-[32rem] hidden dark:block">
+
+        <div class="flex flex-col gap-[1rem] ">
+            <h1 class="font-bold text-4xl text-center lg:text-start lg:text-4xl">
+                <span>Greetings!</span> <span class="whitespace-nowrap">I'm Ahmad Naufan</span>
             </h1>
-            <p class="text-[2rem]">
-                A final year Computer Science student
-                with a passion for programming, particularly in <span class="font-semibold">web development</span>.
+            <p class="text-xl">
+                Full Stack Developer • Bachelor of Computer Science from University of Indonesia
             </p>
-            <a href="#contact"
-               class="text-[1.25rem] italic  w-fit
-                      decoration-secondary underline-offset-8 underline
-                      hover:bg-secondary hover:text-primary">
-                Open to new opportunities
-            </a>
+
+            <div class="flex flex-wrap gap-x-[1rem] gap-y-[1rem]">
+                <span class="flex items-center gap-[0.5rem] w-full md:w-fit me-auto">
+                    <img src="/assets/icons/location.svg" alt="location" class="w-[2rem] md:w-[2rem] dark:hidden">
+                    <img src="/assets/icons/location_light.svg" alt="location"class="w-[2rem] md:w-[2rem] hidden dark:block">
+                    South Tangerang, Indonesia
+                </span>
+                
+                <a href="mailto:anaufan11@gmail.com" target="_blank" class="hover:scale-125 transition duration-500">
+                    <img src="assets/icons/mail.svg" alt="mail" class="w-[2.5rem] dark:hidden">
+                    <img src="assets/icons/mail_light.svg" alt="mail" class="w-[2.5rem] hidden dark:block">
+                </a>
+                <a href="https://www.linkedin.com/in/ahmadnaufanw/" target="_blank" class="hover:scale-125 transition duration-500">
+                    <img src="assets/icons/linkedin.svg" alt="linkedin" class="w-[2.5rem] dark:hidden">
+                    <img src="assets/icons/linkedin_light.svg" alt="linkedin" class="w-[2.5rem] hidden dark:block">
+                </a>
+                <a href="https://github.com/ana117" target="_blank" class="hover:scale-125 transition duration-500">
+                    <img src="assets/icons/github.svg" alt="github" class="w-[2.5rem] dark:hidden">
+                    <img src="assets/icons/github_light.svg" alt="github" class="w-[2.5rem] hidden dark:block">
+                </a>
+            </div>
         </div>
     </div>
     <a href="#content">
