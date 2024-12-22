36daa5074af21e9c0d7628a5c4887f1757a35cd7
ana117 <63540951+ana117@users.noreply.github.com>
2024-12-22 13:41:28 +0700

    responsive error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -14,8 +14,14 @@
 </svelte:head>
 
 <main id="content" class="flex flex-col items-center justify-center gap-[2rem] grow bg-secondary dark:bg-accent">
-  <h1 class="text-4xl font-bold text-center text-primary dark:text-secondary">
-    {page.status} | {errorMessage}
+  <h1 class="text-4xl font-bold text-center text-primary dark:text-secondary flex flex-col md:flex-row gap-[1rem]">
+    <span>
+      {page.status} 
+    </span>
+    <span class="hidden md:inline-block"> | </span>
+    <span>
+      {errorMessage}
+    </span>
   </h1>
 
   <a href="/" class="w-fit px-[2rem] py-[0.5rem] dark:text-secondary hover:bg-accent hover:text-white hover:dark:bg-secondary hover:dark:text-black duration-500 transition-all origin-bottom rounded-md">
