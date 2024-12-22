778e909b0c8ddb81661f50980802e22e9919d175
ana117 <63540951+ana117@users.noreply.github.com>
2024-12-22 13:37:53 +0700

    improve error page styling

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -13,12 +13,12 @@
   <title>ana117 | {page.status} {errorMessage}</title>
 </svelte:head>
 
-<main id="content" class="flex flex-col items-center justify-center gap-[2rem] grow">
-  <h1 class="text-4xl font-bold text-center text-primary">
+<main id="content" class="flex flex-col items-center justify-center gap-[2rem] grow bg-secondary dark:bg-accent">
+  <h1 class="text-4xl font-bold text-center text-primary dark:text-secondary">
     {page.status} | {errorMessage}
   </h1>
 
-  <a href="/" class="w-fit px-[2rem] py-[0.5rem] hover:bg-accent hover:text-white hover:dark:bg-secondary hover:dark:text-black duration-500 transition-all origin-bottom rounded-md">
+  <a href="/" class="w-fit px-[2rem] py-[0.5rem] dark:text-secondary hover:bg-accent hover:text-white hover:dark:bg-secondary hover:dark:text-black duration-500 transition-all origin-bottom rounded-md">
     Go Home
   </a>
 </main>
