accbf7aebeeb917a34968a5bd25a7f98ba7817a2
ana117 <63540951+ana117@users.noreply.github.com>
2024-12-22 13:02:21 +0700

    fix error message

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,22 @@
 <script>
   import { page } from "$app/stores";
+
+  let errorMessage = "Something went wrong";
+  console.log($page.status, typeof $page.status, $page.status === 404);
+  if ($page.status === 404) {
+    errorMessage = "Page Not Found";
+  } else if ($page.error) {
+    errorMessage = $page.error.message;
+  }
 </script>
 
 <svelte:head>
-  <title>ana117 | {$page.status} {$page.error?.message}</title>
+  <title>ana117 | {$page.status} {errorMessage}</title>
 </svelte:head>
 
 <main id="content" class="flex flex-col items-center justify-center gap-[2rem] grow">
   <h1 class="text-4xl font-bold text-center text-primary">
-    {$page.status} {$page.error?.message}
+    {$page.status} | {errorMessage}
   </h1>
 
   <a href="/" class="w-fit px-[2rem] py-[0.5rem] hover:bg-accent hover:text-white hover:dark:bg-secondary hover:dark:text-black duration-500 transition-all origin-bottom rounded-md">
