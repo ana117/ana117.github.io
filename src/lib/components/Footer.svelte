5575f89743cd0e71b43b5a709b7f4d5089407e66
ana117 <63540951+ana117@users.noreply.github.com>
2025-08-03 21:04:35 +0700

    update year

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <footer class="py-[2rem] md:ml-[8rem] bg-secondary text-center dark:bg-accent dark:text-secondary">
     <p class="text-bg font-semibold text-xl md:text-lg">
-        © 2024 Ahmad Naufan W
+        © 2025 Ahmad Naufan W
     </p>
 </footer>
